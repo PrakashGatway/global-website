44b0d5b5ca253cc94738136c32737450e0b0fdda
gatewayabroadweb-a11y <naveengoswami309@gmail.com>
2026-04-09 16:42:56 +0530

    changes

--- components/new.tsx
+++ components/new.tsx
@@ -392,7 +392,9 @@ export default function UniDetailsClient({
                       : "border-transparent text-gray-500 hover:text-black"
                   }`}
                 >
-                  {section.heading}
+                  <span dangerouslySetInnerHTML={{
+                    __html : section.heading
+                  }}/>
                 </button>
               ))}
             </div>
@@ -413,9 +415,11 @@ export default function UniDetailsClient({
               }}
               className="scroll-mt-28"
             >
-              <h2 className="text-2xl font-bold text-slate-900 mb-4">
-                {section.heading}
-              </h2>
+              <h2 className="text-2xl font-bold text-slate-900 mb-4"
+              dangerouslySetInnerHTML={{
+              __html :  section.heading
+            }}  
+              />
               <div
                 className="prose max-w-none text-slate-700 
              prose-headings:text-slate-900
